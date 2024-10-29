bab71a0b8361884e11a5cef793bb64de18ea6583
Frez119 <o3o_chol@naver.com>
2024-10-29 11:04:25 +0900

    ,

--- hapcahe.py
+++ hapcahe.py
@@ -1,12 +1,9 @@
 from gpiozero import Robot, DistanceSensor
-from gpiozero.pins.pigpio import PiGPIOFactory
 import time
 
-# GPIO 핀 팩토리 설정
-factory = PiGPIOFactory()
 
 # 거리 센서 설정 (GPIO 23은 트리거, GPIO 24는 에코)
-sensor = DistanceSensor(echo=24, trigger=23, pin_factory=factory)
+sensor = DistanceSensor(echo=24, trigger=23)
 
 # DC 모터 설정
 dc_motor = Robot(left=(6, 12), right=(11, 9))
